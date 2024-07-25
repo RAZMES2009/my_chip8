e9f2657cc89a6dc081faebc46cc8c9b5dc1d4580
razmes2009 <semin.02@mail.ru>
2024-07-25 16:52:16 +0300

    init

---
+++ src/chip8.hpp
@@ -0,0 +1,53 @@
+#pragma once
+
+#include <random>
+#include <cstdint>
+
+const unsigned int VIDEO_WIDTH = 64;
+const unsigned int VIEDO_HEIGHT = 32;
+
+class Chip8
+{
+public:
+    uint8_t registers[16]{};
+    uint8_t memory[4096]{};
+    uint16_t index{};
+    uint16_t pc{};
+    uint16_t stack[16]{};
+    uint8_t sp{};
+    uint8_t delayTimer{};
+    uint8_t soundTimer{};
+    uint8_t keypad[16]{};
+    uint32_t video[64 * 32]{};
+    uint16_t opcode{};
+    std::default_random_engine randGen;
+    std::uniform_int_distribution<uint8_t> randByte;
+
+    Chip8();
+    void LoadROM(char const *);
+
+    void OP_00E0();
+    void OP_00EE();
+    void OP_1nnn();
+    void OP_2nnn();
+    void OP_3xkk();
+    void OP_4xkk();
+    void OP_5xy0();
+    void OP_6xkk();
+    void OP_7xkk();
+    void OP_8xy0();
+    void OP_8xy1();
+    void OP_8xy2();
+    void OP_8xy3();
+    void OP_8xy4();
+    void OP_8xy5();
+    void OP_8xy6();
+    void OP_8xy7();
+    void OP_8xyE();
+    void OP_9xy0();
+    void OP_Annn();
+    void OP_Bnnn();
+    void OP_Cxkk();
+    void OP_Dxyn();
+    
+};
